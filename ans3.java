512de4f8798ebc956e26af2a1b20aafa81c7a439
Abhishek Dixit <abhishek.dixitknp111@gmail.com>
2023-06-19 15:33:05 +0530

    Add files via upload

---
+++ ans3.java
@@ -0,0 +1,32 @@
+
+// 💡  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+
+import java.util.*;
+
+public class ans3 {
+    
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid-1;
+            else start = mid+1;
+        }
+
+        return start;
+    }
+    public static void main(String args[]){
+        int []nums = {1,3,5,6};
+        int target = 5;
+       
+        System.out.println(searchInsert(nums,target));
+    }
+}
+    
