57b18692bf1073c9c185ca9365b4c5fe05b0f1d5
Abhishek Dixit <abhishek.dixitknp111@gmail.com>
2023-06-19 16:33:09 +0530

    Add files via upload

---
+++ ans6.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Arrays;
+ 
+class ans6{
+    
+    public boolean containsDuplicate(int[] nums) {
+ 
+        
+        for(int index = 1; index < nums.length; index++) {
+            if(nums[index ] == nums[index - 1]) {
+                return true;
+            }
+        }
+ 
+        return false;
+    }
+    
+    public static void main (String[] args) {
+	    
+	Solution sol= new Solution();
+	    
+	int nums[]= {1, 2, 3, 1};
+	    
+	boolean res = sol.containsDuplicate(nums);
+	    
+	System.out.println(res);
+    }
+}
