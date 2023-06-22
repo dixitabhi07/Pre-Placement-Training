50e92fbd7f063339db6cd8a2f7cec61bb1023a2e
Abhishek Dixit <abhishek.dixitknp111@gmail.com>
2023-06-22 16:00:11 +0530

    Add files via upload

---
+++ ans8.java
@@ -0,0 +1,27 @@
+// Question 8
+// Given an array of meeting time intervals where intervals[i] = [starti, endi],
+// determine if a person could attend all meetings.
+
+import java.util.*;
+class ans8 {
+
+  public static boolean canAttendMeetings(int[][] intervals) {
+    Arrays.sort(intervals, new Comparator<int[]>() {
+      public int compare(int[] i1, int[] i2) {
+        return i1[0] - i2[0];
+      }
+    });
+
+    for (int i = 0; i < intervals.length - 1; i++) {
+      if (intervals[i][1] > intervals[i + 1][0])
+        return false;
+    }
+    return true;
+  }
+  public static void main(String args[]){
+   
+       int [][] intervals = {{0,30},{5,10},{15,20}};
+       
+       System.out.println(canAttendMeetings(intervals));
+    }
+}
