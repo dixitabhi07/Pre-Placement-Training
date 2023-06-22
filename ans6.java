8ad14e9ed9a35847100b6d002a3b98e4a805091f
Abhishek Dixit <abhishek.dixitknp111@gmail.com>
2023-06-22 15:44:40 +0530

    Add files via upload

---
+++ ans6.java
@@ -0,0 +1,18 @@
+//answer 6
+import java.util.*;
+public class ans6 {
+   
+    public static  int singleNumber(int[] nums) {
+        int ans =0;
+        for(int i=0;i<nums.length;i++)  ans=ans^nums[i];
+        return ans;
+ 
+    }
+     public static void main(String args[]){
+       int[] nums = {2,2,1};
+       
+       System.out.println(singleNumber(nums));
+    }
+}
+    
+
